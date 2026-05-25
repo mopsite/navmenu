9a937139217207589d7382f6f8d35c2587aa2a82
Paul <37662788@qq.com>
2026-05-25 21:51:47 +0800

    navmenu

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/navmenu/' : '/',
+  base: process.env.GITHUB_PAGES ? '/navmenu/' : '/',
   build: {
     assetsDir: 'assets',
     outDir: 'dist'
